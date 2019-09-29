b7788ac2368252b4fb25fe8db9c33de779908815
Willem de Groot <willembike@gmail.com>
2019-09-29 21:30:36 +0200

    Rewrote how parameters are received, specified default case, expanded help menu, added comments, updated input handling to avoid exceptions.

--- Program.cs
+++ Program.cs
@@ -6,62 +6,175 @@ namespace IntThreat
 {
     class Program
     {
+        enum TestMode { None, Single, Increment };
+        enum ResultMode { None, Console, File };
         static void Main(string[] args)
         {
             string file = "";
-            if (args.Length == 3 && args[1] == "-f")
+            int threads = 0;
+            TestMode testmode = TestMode.None;
+            ResultMode resultmode = ResultMode.None;
+
+            // reading user input
+            if (args.Length == 0) // default option
             {
-                file = args[2];
+                threads = 4;
+                testmode = TestMode.Single;
+                resultmode = ResultMode.Console;
             }
-            if (args.Length != 0 && (args[0] == "-h" || args[0] == "help"))
+            for (int i = 0; i < args.Length; ++i) // go through all parameters
             {
-                Console.WriteLine("Use '-h' or 'help' for this menu");
-                Console.WriteLine("Use '-s' to run a single test");
-                Console.WriteLine("Use '-i' to run a test for each number of threads until given number");
-                Console.WriteLine("Use '-f' followed by file name to save result to given file. Must follow '-i' or '-s'");
-            }
-            else if (args.Length == 0 || args[0] == "-s")
-            {
-                int count = AskNrThreads();
-                Console.Write("Time for " + count + " threads: ");
-                TimeSpan duration = Intrun(count);
-                if (file == "")
-                    Console.WriteLine(duration);
-                else
+                // TODO: add option for console output (explicitly), add option for repeating same test, and appending or overwriting file
+                if (args[i] == "-h" || args[i] == "help") // help menu
                 {
-                    System.IO.File.WriteAllText(file, count + ". " + duration + "\n");
+                    if (i != 0) // help should be first parameter, possibly followed by the parameter you need help with
+                    {
+                        Console.WriteLine("Invalid command sequence");
+                        return;
+                    }
+                    else if (args.Length == 1) // only display default help menu
+                    {
+                        Console.WriteLine("Use '-h' or 'help' for help menu");
+                        Console.WriteLine("Use '-s' or 'single' to run a single test for give amount of threads");
+                        Console.WriteLine("Use '-i' or 'increment' to run a test for each number of threads until given amount of threads");
+                        Console.WriteLine("Use '-f' or 'file' to save output in given file.");
+                        Console.WriteLine("For more info about a specific command, use '" + args[i] + " <command>'");
+                        return;
+                    }
+                    else if (args[i + 1] == "-s" || args[i + 1] == "single")
+                    {
+                        Console.WriteLine("This will run a single test for a given number of threads.");
+                        Console.WriteLine("Specify the number of threads after the command, for example:");
+                        Console.WriteLine("'-s 8'");
+                        return;
+                    }
+                    else if (args[i + 1] == "-i" || args[i + 1] == "increment")
+                    {
+                        Console.WriteLine("This will run a series of tests, from 1 thread to the give amount of threads.");
+                        Console.WriteLine("Specify the number of threads after the command, for example:");
+                        Console.WriteLine("'-i 8'");
+                        Console.WriteLine("This will run the test first with 1 thread, then 2, and so on \nuntil the specified number is reached (8 in this case)");
+                        return;
+                    }
+                    else if (args[i + 1] == "-f" || args[i + 1] == "file")
+                    {
+                        Console.WriteLine("This will save the test results in a file instead of printing it to the console.");
+                        Console.WriteLine("Specify the file name of the file in which the result needs to be saved.");
+                        Console.WriteLine("If the file already exists, it will be overwritten");
+                        return;
+                    }
+                    else // invalid command
+                    {
+                        Console.WriteLine("'" + args[i + 1] + "' is not a recognised command. \nPlease use '-h' or 'help' without further parameters to see the currently supported commands.");
+                        return;
+                    }
+                }
+                else if (args[i] == "-s" || args[i] == "single") // follow this by number of cores and run test once
+                {
+                    if (args.Length == i + 1)
+                    {
+                        Console.WriteLine("Missing parameter for '" + args[i] + "'");
+                        return;
+                    }
+                    try
+                    {
+                        threads = int.Parse(args[i + 1]);
+                        testmode = TestMode.Single;
+                        ++i;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("'" + args[i + 1] + "' is not a valid number of cores.");
+                        return;
+                    }
+                }
+                else if (args[i] == "-i" || args[i] == "increment") // follow this by number of cores and test from 1 core to n cores
+                {
+                    if (args.Length == i + 1)
+                    {
+                        Console.WriteLine("Missing parameter for '" + args[i] + "'");
+                        return;
+                    }
+                    try
+                    {
+                        threads = int.Parse(args[i + 1]);
+                        testmode = TestMode.Increment;
+                        ++i;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("'" + args[i + 1] + "' is not a valid number of cores.");
+                        return;
+                    }
+                }
+                else if (args[i] == "-f" || args[i] == "file") // follow this by the file path
+                {
+                    if (args.Length == i + 1)
+                    {
+                        Console.WriteLine("Missing parameter for '" + args[i] + "'");
+                        return;
+                    }
+                    file = args[i + 1];
+                    resultmode = ResultMode.File;
+                    ++i;
+                }
+                else if (args[i] == "-p" || args[i] == "echo")
+                {
+                    resultmode = ResultMode.Console;
+                }
+                else // invalid command
+                {
+                    // maybe just ignore?
+                }
+            }
+
+            if (testmode == TestMode.None)
+            {
+                Console.WriteLine("No test mode specified.");
+                return;
+            }
+            if (resultmode == ResultMode.None)
+            {
+                resultmode = ResultMode.Console;
+            }
+
+            // carrying out tests
+            if (testmode == TestMode.Single)
+            {
+                if (resultmode == ResultMode.Console)
+                {
+                    Console.Write("Time for " + threads + " threads: ");
+                    Console.WriteLine(Intrun(threads));
+                }
+                else if (resultmode == ResultMode.File)
+                {
+                    Console.WriteLine("Testing for " + threads + " threads");
+                    System.IO.File.WriteAllText(file, threads + ". " + Intrun(threads) + "\n");
                     Console.WriteLine("Results exported to " + file);
                 }
             }
-            else if (args.Length != 0 && args[0] == "-i")
+            else if (testmode == TestMode.Increment)
             {
-                int count = AskNrThreads();
-                if (file == "")
-                    for (int i = 1; i <= count; ++i)
+                if (resultmode == ResultMode.Console)
+                {
+                    Console.WriteLine("Results per threads:");
+                    for (int i = 1; i <= threads; ++i)
                     {
                         Console.Write(i + " threads: ");
                         Console.WriteLine(Intrun(i));
                     }
-                else
+                }
+                else if (resultmode == ResultMode.File)
                 {
+                    Console.WriteLine("Testing from 1 to " + threads + " threads");
                     System.IO.File.WriteAllText(file, "");
-                    for (int i = 1; i <= count; ++i)
+                    for (int i = 1; i <= threads; ++i)
                         System.IO.File.AppendAllText(file, i + ". " + Intrun(i) + "\n");
                     Console.WriteLine("Results exported to " + file);
                 }
             }
         }
-
-        public static int AskNrThreads()
-        {
-            Console.Write("Choose number of threads: ");
-            string input = Console.ReadLine();
-            int count;
-            try { count = int.Parse(input); }
-            catch { throw new InvalidCastException("Incorrect input, \"" + input + "\" is not a number."); }
-            return count;
-        }
-        public static TimeSpan Intrun(int count)
+        public static TimeSpan Intrun(int count) // This is where the magic happens
         {
             List<Thread> threads = new List<Thread>();
             DateTime start, end;
@@ -77,7 +190,7 @@ namespace IntThreat
             end = DateTime.Now;
             return (end - start);
         }
-        public static void onerun()
+        public static void onerun() // This is where the hard work is done
         {
             int counter = 0;
             while (++counter != 0) ;
